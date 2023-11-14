a179d3325cd35f2109dac52b3cc057a11b8c988f
catriona beamish <mani.motamedifr@gmail.com>
2023-11-14 14:54:40 +0100

    Noice

--- config/routes.rb
+++ config/routes.rb
@@ -7,4 +7,12 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "posts#index"
+  get "/tasks", to: "tasks#list", as: "tasks"
+  # "/tasks/new" needs to be on top of "/tasks/:id" in order for us not to get an error coz otherwise it will see the unique identifier (:id) and will pass it to see the /new
+  get "/tasks/new", to: "tasks#new", as: "task_new"
+  get "/tasks/:id", to: "tasks#task", as: "task"
+  post "/tasks", to: "tasks#create"
+  get "/tasks/:id/edit", to: "tasks#edit", as: "task_edit"
+  patch "tasks/:id", to: "tasks#update"
+  delete "tasks/:id", to: "tasks#destroy"
 end
